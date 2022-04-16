fdc5cc1be2c145239aad7f1975d3ce64afb48d61
Shahzad <mech1090@gmail.com>
2022-04-16 09:50:52 -0600

    added footer and header

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
 const pug = require('pug')
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
     res.send('OK')
 })
 
 app.set('view engine', 'pug')
 app.set('views','./views')
-app.get('/pugDemo',(req,res)=>{
-    res.render('index',{
+app.get('pugDemo',(req,res)=>{
+    res.render('/pugDemo/layout',{
         pageTitle :'Express Demo',
         pageHeader : 'Hello Express',
         isAuthenticated : true
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is running on 3000')
+app.listen(3100,()=>{
+    console.log('Server is running on 3100')
 })
